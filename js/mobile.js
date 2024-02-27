24580236a3d78a186fb15112b4739f839cec1635
Shahab Uddin <shahab.82946@gmail.com>
2024-02-27 23:47:20 +0600

    added mobilev caard

--- js/mobile.js
+++ js/mobile.js
@@ -1,1 +1,36 @@
-console.log('hello');
+const loadPhone = async (searchfield) =>{
+  const res = await fetch (`https://openapi.programming-hero.com/api/phones?search=${searchfield}`);
+  const data = await res.json();
+  const phone = data.data;
+  displayPhones(phone);
+}
+
+
+const displayPhones = phones => {
+const phoneContainer = document.getElementById('phone-container');
+
+phoneContainer.textContent = '';
+  phones.forEach(phone =>{
+    const phoneCard = document.createElement('div');
+    phoneCard.classList = `card w-96 bg-gray-100 p-4 shadow-xl`;
+    phoneCard.innerHTML = `<figure><img src="${phone.image}" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">${phone.phone_name}</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div> `;
+
+      phoneContainer.appendChild(phoneCard);
+
+    
+  });
+}
+
+const clickHandlers = () => {
+  const searchButton = document.getElementById('search-button')
+  const searchfield = searchButton.value;
+  loadPhone(searchfield);
+
+}
+
